166865c430922f5a4257e0fa3b6bdbbd946a0f8b
lethanhdo <thanhdochita@gmail.com>
2021-01-15 23:31:20 +0700

    add to git

---
+++ style.js
@@ -0,0 +1,21 @@
+$( "#button1" ).click(function() {
+    // $( "#button1" ).html('Có')
+    $("#heart").toggleClass("heart-show")
+    $( ".card" ).toggleClass("card-hidden")
+  });
+
+  $( ".btn-back" ).click(function() {
+    // $( "#button1" ).html('Có')
+    $("#heart").toggleClass("heart-show")
+    $( ".card" ).toggleClass("card-hidden")
+  });
+
+  $( "#button2" ).click(function() {
+    $( ".button-box" ).toggleClass("fix")
+  });
+
+  $( ".i-close" ).click(function() {
+    alert("Answer me before out !!!")
+   console.log('hi')
+   });
+   
